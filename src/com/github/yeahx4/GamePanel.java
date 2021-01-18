c913930216f69ef21703e08c63a57cc104e8c0e4
5tarlight <developerstarlight@gmail.com>
2021-01-18 21:14:25 +0900

    finalize

--- src/com/github/yeahx4/GamePanel.java
+++ src/com/github/yeahx4/GamePanel.java
@@ -14,14 +14,14 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = SCREEN_WIDTH * SCREEN_HEIGHT / UNIT_SIZE;
     static final int DELAY = 75;
+    final Random random;
+    final Point apple;
     final Point[] pos = new Point[GAME_UNITS];
     int bodyParts = 6;
     int applesEaten = 0;
-    Point apple;
     SnakeDriection direction = SnakeDriection.RIGHT;
     boolean running = false;
     Timer timer;
-    Random random;
 
     GamePanel() {
         random = new Random();
